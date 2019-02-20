3503734c2c5a84d75006a1682cab75d2b6a3a099
Vedant <vedant.lohbare@gmail.com>
2019-02-20 14:13:45 +0000

    Built the edit function

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,5 +18,19 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  def update
+   @portfolio_item = Portfolio.find(params[:id])
+   
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html {redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
 end
