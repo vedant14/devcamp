913ddcc10093a450c110bad7ec365b0c61648495
Vedant <vedant.lohbare@gmail.com>
2019-02-25 14:49:11 +0000

    Implemented destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -32,5 +32,14 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-
+  def show
+   @portfolio_item = Portfolio.find(params[:id])
+  end
+  def destroy
+  @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully destroyed.' }
+    end
+  end
 end
