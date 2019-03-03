2f65c885bf8dee93690f07fb1ccf2860b2a5bc1c
Vedant <vedant.lohbare@gmail.com>
2019-03-03 03:12:30 +0000

    Implemented custom action for blogs

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  resources :blogs do
+    member do 
+      get :toggle_status
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
