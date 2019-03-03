9827e60a8a180357b3acacf5b4dcdde8b7bbfa6c
Vedant <vedant.lohbare@gmail.com>
2019-03-03 07:03:04 +0000

    validated data for portfolio, blog and skills

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ApplicationRecord
   enum status: {published: 1, draft: 0}
   extend FriendlyId
   friendly_id :title, use: :slugged
-  
+  validates_presence_of :title, :body
  def should_generate_new_friendly_id?
    title_changed?
  end
