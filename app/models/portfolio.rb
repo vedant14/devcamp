b36d2277dd89e1f199df2b9865c349877822074b
Vedant <vedant.lohbare@gmail.com>
2019-03-12 14:45:43 +0000

    Nested attributes completed

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,6 +2,9 @@ class Portfolio < ApplicationRecord
   has_many :technologies
   validates_presence_of :title,:body, :main_image, :thumb_image
   include Placeholder
+  accepts_nested_attributes_for :technologies,
+                                      reject_if: lambda { |attrs| attrs['name'].blank?}
+
   
   def self.angular
     where(subtitle: "Angular")
