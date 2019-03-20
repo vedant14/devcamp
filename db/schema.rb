a33e5e0f36b0d98e5e6ce94b9f6d051e9ccadaa5
Vedant <vedant.lohbare@gmail.com>
2019-03-20 16:02:34 +0000

    Installed the petergate gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_175416) do
+ActiveRecord::Schema.define(version: 2019_03_20_160113) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2019_03_13_175416) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
