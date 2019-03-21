806a996532356085d68657d080791aeb3184b058
Vedant <vedant.lohbare@gmail.com>
2019-03-21 17:58:52 +0000

    Implemented index styles for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
-  access all: [:show, :index], user: {except: [:destroy, :new, :update, :create, :edit]}, company_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :update, :create, :edit, :toggle_status
+  ]}, site_admin: :all
   layout 'blog'
   # GET /blogs
   # GET /blogs.json
