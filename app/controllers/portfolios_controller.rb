13223a2267ff3d7c70aedbe2b264b5cf2598e30d
Vedant <vedant.lohbare@gmail.com>
2019-03-31 06:06:33 +0000

    Refactored the alerts

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,7 +40,7 @@ class PortfoliosController < ApplicationController
    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html {redirect_to portfolios_path, notice: 'Record was successfully updated.' }
+        format.html {redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
       else
         format.html { render :edit }
       end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,15 @@ module ApplicationHelper
   def active? path
   "active" if current_page? path
   end
+  
+  
+  def alerts
+    alert = ( flash[:alert] || flash[:error] || flash[:notice])  
+    
+    alert_generator alert
+  end
+  
+  def alert_generator msg
+    js add_gritter(msg, title: "Alert!", :time => 1500)
+  end
 end
