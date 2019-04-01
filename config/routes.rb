28c0afc6f3632c167705b0d7e510f1484697327c
Vedant <vedant.lohbare@gmail.com>
2019-04-01 16:19:27 +0000

    Created a comments broadcast

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+  
+  mount ActionCable.server => '/cable'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
